93132f0312959f5ec310a2dbf85cc04e7a687226
Dimitar <dimitar.mandarliev@gmail.com>
2024-05-12 18:19:13 +0300

    Header

--- app/header.tsx
+++ app/header.tsx
@@ -1,22 +1,73 @@
 "use client";
 
 import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogInIcon, LogOutIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import Link from "next/link";
 
-export function Header() {
+function AccountDropdown() {
   const session = useSession();
+  const isLoggedIn = !!session.data;
 
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"link"}>
+          <Avatar className="mr-2">
+            <AvatarImage
+              src={session.data?.user.image ?? ""}
+              height={50}
+              width={50}
+              alt="Avatar"
+            />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon className="mr-2" />
+            Sign Out
+          </DropdownMenuItem>
         ) : (
-          <Button onClick={() => signIn("google")}>Sign In</Button>
+          <DropdownMenuItem onClick={() => signIn("google")}>
+            <LogInIcon className="mr-2" />
+            Sign In
+          </DropdownMenuItem>
         )}
-        {session.data?.user?.name}
-        <ModeToggle />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function Header() {
+  return (
+    <header className="bg-gray-100 py-2 mx-auto dark:bg-gray-900 relative">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="flex gap-2 items-center text-xl hover:underline">
+          <Image
+            src="/icon.png"
+            width={60}
+            height={60}
+            alt="the application icon of a magnifying glass"
+          />
+          DevFinder
+        </Link>
+        <div className="flex items-center gap-4">
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
